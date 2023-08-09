9bc7695585c9f01a538b6404bef3054beacbaf8f
yejiiha <1126yezy@gmail.com>
2023-08-09 22:28:57 +0900

    setting : styled-components 설정

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+export const GlobalStyle = createGlobalStyle`
+    ${reset};
+
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        overflow-x: hidden;
+    }
+
+    a {
+        all: unset;
+    }
+
+    input, textarea {
+        outline: none;
+        border: none;
+    }
+
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;             
+        margin: 0;         
+    }
+
+    button {
+        cursor: pointer;
+        outline: none;
+        border: none;
+        background-color: inherit;
+
+        &:disabled {
+            cursor: not-allowed;
+        }
+    }
+`;
