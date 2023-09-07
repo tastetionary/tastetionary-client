0a68641628c06519dc2b4b432d6861277d4c2de7
yejiiha <1126yezy@gmail.com>
2023-09-07 09:57:01 +0900

    [KB-56] feat: Text Input2 컴포넌트 추가

---
+++ src/components/Input/TextInput/index.tsx
@@ -0,0 +1,36 @@
+import INPUT_ERROR from '@/assets/Input/InputError.svg';
+import INPUT_INACTIVE from '@/assets/Input/InputInactive.svg';
+import INPUT_TYPED from '@/assets/Input/InputTyped.svg';
+import { useState } from 'react';
+import * as S from './page.styled';
+
+interface TextInputProps {
+  label?: string;
+  errorMsg?: string;
+  [key: string]: any;
+}
+
+export default function TextInput({ label, errorMsg, ...rest }: TextInputProps) {
+  const [focused, setFocused] = useState(false);
+
+  const hasError = Boolean(errorMsg);
+
+  return (
+    <S.Container>
+      {label && <S.Label>{label}</S.Label>}
+
+      <S.InputContainer
+        $focused={focused}
+        $hasError={hasError}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      >
+        <S.Input $hasError={hasError} {...rest} />
+
+        {hasError ? <INPUT_ERROR /> : focused ? <INPUT_TYPED /> : <INPUT_INACTIVE />}
+      </S.InputContainer>
+
+      {errorMsg && <S.ErrorMsg>{errorMsg}</S.ErrorMsg>}
+    </S.Container>
+  );
+}
