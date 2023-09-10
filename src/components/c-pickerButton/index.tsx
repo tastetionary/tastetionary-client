ad191e6e64c5f8800a0245de423c3117a92f419b
hovelopin <kimhojin3714@naver.com>
2023-09-10 19:14:12 +0900

    [KB-52] fix : pickerButton 로직 및 css 수정

--- src/components/c-pickerButton/index.tsx
+++ src/components/c-pickerButton/index.tsx
@@ -1,5 +1,7 @@
 import MenuIcon from '@/assets/logo/menu_icon.svg';
+import RestaurantIcon from '@/assets/logo/restaurant_icon.svg';
 import { styled } from 'styled-components';
+import HorizontalLayout from '../layout/horizontal-layout';
 
 interface Props {
   title: string;
@@ -10,36 +12,47 @@ interface Props {
 export default function CPickerButton({ title, desc, subject }: Props) {
   return (
     <>
-      <S.Button>
-        <S.Header></S.Header>
-        <S.Content>
-          <S.Icon>{subject === 'menu' && <MenuIcon />}</S.Icon>
-          <S.Title>{title}</S.Title>
-          <S.Description subject={subject}>{desc}</S.Description>
-        </S.Content>
+      <S.Button subject={subject}>
+        <HorizontalLayout subject={subject}>
+          <S.Header></S.Header>
+          <S.Content>
+            <S.Icon>
+              {subject === 'menu' && <MenuIcon />}
+              {subject === 'restaurant' && <RestaurantIcon />}
+            </S.Icon>
+            <S.Title>{title}</S.Title>
+            <S.Description subject={subject}>{desc}</S.Description>
+          </S.Content>
+        </HorizontalLayout>
       </S.Button>
     </>
   );
 }
 
 const S = {
-  Button: styled.button`
+  Button: styled.button<{ subject: 'menu' | 'restaurant' }>`
     width: 280px;
     box-shadow: 4px 4px 0px 0px rgba(0, 0, 0, 0.1);
     padding: 0px 0px;
+    background: repeating-linear-gradient(
+      ${({ theme, subject }) =>
+        subject === 'menu'
+          ? `${theme.colors.secondary.o10}, ${theme.colors.secondary.o10} 20px, ${theme.colors.secondary.o05} 0, ${theme.colors.secondary.o05} 100%`
+          : `${theme.colors.primary.y20}, ${theme.colors.primary.y20} 20px, ${theme.colors.primary.y05} 0, ${theme.colors.primary.y05} 100%`}
+    );
   `,
   Header: styled.div`
     width: 280px;
     height: 20px;
-    background-color: ${props => props.theme.colors.secondary.o10};
   `,
-  Content: styled.div`
-    background-color: ${props => props.theme.colors.secondary.o05};
-  `,
+  Content: styled.div``,
   Icon: styled.div``,
-  Title: styled.p``,
-  Description: styled.p<{ subject: string }>`
+  Title: styled.p`
+    font-size: 20px;
+  `,
+  Description: styled.p<{ subject: 'menu' | 'restaurant' }>`
     color: ${({ subject, theme }) => (subject === 'menu' ? theme.colors.secondary.o30 : theme.colors.primary.y50)};
-    padding: 15px 0px 17px 0px;
+    padding: 19px 0px 17px 0px;
+    font-size: 14px;
   `,
 };
