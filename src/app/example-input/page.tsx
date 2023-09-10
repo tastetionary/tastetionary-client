acddeefbba3301470c54331baad72f67b29dc2a2
yejiiha <1126yezy@gmail.com>
2023-09-10 11:00:16 +0900

    [KB-57] fix: 컴포넌트트 type 수정

--- src/app/example-input/page.tsx
+++ src/app/example-input/page.tsx
@@ -2,7 +2,7 @@
 
 import MainButton from '@/components/Button/MainButton';
 import TextInput from '@/components/Input/TextInput';
-import { SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 export default function ExampleInput() {
@@ -10,24 +10,20 @@ export default function ExampleInput() {
 
   return (
     <Main>
-      <TextInput
-        placeholder="아이디를 입력해 주세요"
-        value={text}
-        onChange={(e: { target: { value: SetStateAction<string> } }) => setText(e.target.value)}
-      />
+      <TextInput placeholder="아이디를 입력해 주세요" value={text} onChange={e => setText(e.target.value)} />
 
       <TextInput
         label="아이디"
         placeholder="아이디를 입력해 주세요"
         value={text}
-        onChange={(e: { target: { value: SetStateAction<string> } }) => setText(e.target.value)}
+        onChange={e => setText(e.target.value)}
       />
 
       <TextInput
         errorMsg="ERROR TEXT ERROR TEXT ERROR"
         placeholder="아이디를 입력해 주세요"
         value={text}
-        onChange={(e: { target: { value: SetStateAction<string> } }) => setText(e.target.value)}
+        onChange={e => setText(e.target.value)}
       />
 
       <MainButton btnText="테스트" />
