6f13a3ede7d4030ec3ce639ac8ade20d26f98104
hovelopin <kimhojin3714@naver.com>
2023-09-11 19:48:15 +0900

    [KB-52] feat : 메뉴/식당 추첨 버튼 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import CHeader from '@/components/c-header';
+import CPickerButton from '@/components/c-pickerButton';
+import GridLayout from '@/components/layout/grid-layout';
+import * as S from './page.styled';
 
 export default function Home() {
   return (
     <>
-      <CHeader title="맛셔너리" isLogo isBackBtn />
+      <CHeader title="맛셔너리" isLogo />
+      <GridLayout>
+        <S.MainContent>
+          <CPickerButton title={'메뉴 고르기'} desc={'오늘은 어떤 음식을 먹을까?'} subject={'menu'} />
+          <CPickerButton title={'식당 고르기'} desc={'오늘은 어떤 식당에 가볼까?'} subject={'restaurant'} />
+        </S.MainContent>
+      </GridLayout>
     </>
   );
 }
--- src/components/c-pickerButton/index.tsx
+++ src/components/c-pickerButton/index.tsx
@@ -33,13 +33,22 @@ const S = {
   Button: styled.button<{ subject: 'menu' | 'restaurant' }>`
     width: 280px;
     box-shadow: 4px 4px 0px 0px rgba(0, 0, 0, 0.1);
-    padding: 0px 0px;
+    padding: 0px;
     background: repeating-linear-gradient(
       ${({ theme, subject }) =>
         subject === 'menu'
           ? `${theme.colors.secondary.o10}, ${theme.colors.secondary.o10} 20px, ${theme.colors.secondary.o05} 0, ${theme.colors.secondary.o05} 100%`
           : `${theme.colors.primary.y20}, ${theme.colors.primary.y20} 20px, ${theme.colors.primary.y05} 0, ${theme.colors.primary.y05} 100%`}
     );
+
+    &:active {
+      background: repeating-linear-gradient(
+        ${({ theme, subject }) =>
+          subject === 'menu'
+            ? `${theme.colors.secondary.o40}, ${theme.colors.secondary.o40} 20px, ${theme.colors.secondary.o05} 0, ${theme.colors.secondary.o05} 100%`
+            : `${theme.colors.primary.y40}, ${theme.colors.primary.y40} 20px, ${theme.colors.primary.y05} 0, ${theme.colors.primary.y05} 100%`}
+      );
+    }
   `,
   Header: styled.div`
     width: 280px;
