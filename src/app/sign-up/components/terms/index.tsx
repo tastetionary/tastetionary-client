eb0fb27719af058d4ca9b5d1a502d94bf17b20a5
hovelopin <kimhojin3714@naver.com>
2023-09-16 12:40:45 +0900

    [KB-83] feat : title , subTitle 작업 완료

--- src/app/sign-up/components/terms/index.tsx
+++ src/app/sign-up/components/terms/index.tsx
@@ -1,9 +1,19 @@
 import CHeader from '@/components/c-header';
+import * as S from './page.styled';
 
 export default function Terms() {
   return (
     <>
       <CHeader title="약관 동의" isBackBtn />
+      <S.Wrapper>
+        <S.Title>
+          서비스 이용을 위해 <br />
+          아래 약관에 동의해주세요.
+        </S.Title>
+        <S.SubTitle>
+          서비스 이용을 위해 동의가 필요합니다. <br /> 정책 및 약관을 확인 후 회원가입을 진행해주세요.
+        </S.SubTitle>
+      </S.Wrapper>
     </>
   );
 }
