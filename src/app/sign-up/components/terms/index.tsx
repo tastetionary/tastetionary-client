86dde650c4d17843731a8cb0fe8dfd1c3ea90995
hovelopin <kimhojin3714@naver.com>
2023-09-17 16:38:04 +0900

    [KB-83] feat : 이용약관 전체 동의 UI 작업 완료

--- src/app/sign-up/components/terms/index.tsx
+++ src/app/sign-up/components/terms/index.tsx
@@ -1,3 +1,4 @@
+import CheckBox2 from '@/components/CheckBox/CheckBox2';
 import CHeader from '@/components/c-header';
 import * as S from './page.styled';
 
@@ -6,13 +7,26 @@ export default function Terms() {
     <>
       <CHeader title="약관 동의" isBackBtn />
       <S.Wrapper>
-        <S.Title>
-          서비스 이용을 위해 <br />
-          아래 약관에 동의해주세요.
-        </S.Title>
-        <S.SubTitle>
-          서비스 이용을 위해 동의가 필요합니다. <br /> 정책 및 약관을 확인 후 회원가입을 진행해주세요.
-        </S.SubTitle>
+        <S.TitleContainer>
+          <S.Title>
+            서비스 이용을 위해 <br />
+            아래 약관에 동의해주세요.
+          </S.Title>
+          <S.SubTitle>
+            서비스 이용을 위해 동의가 필요합니다. <br /> 정책 및 약관을 확인 후 회원가입을 진행해주세요.
+          </S.SubTitle>
+        </S.TitleContainer>
+        <S.CheckboxWrapper>
+          <S.Divider></S.Divider>
+          <S.CheckboxContainer>
+            <CheckBox2
+              checkBoxId="terms-of-agree"
+              label="이용 약관 전체 동의"
+              onChangeEvent={() => console.log('click')}
+              checked={false}
+            />
+          </S.CheckboxContainer>
+        </S.CheckboxWrapper>
       </S.Wrapper>
     </>
   );
--- src/app/sign-up/components/terms/page.styled.ts
+++ src/app/sign-up/components/terms/page.styled.ts
@@ -4,6 +4,8 @@ export const Wrapper = styled.div`
   padding: 20px;
 `;
 
+export const TitleContainer = styled.div``;
+
 export const Title = styled.p`
   font-size: 20px;
   font-weight: 700;
@@ -17,4 +19,24 @@ export const SubTitle = styled.p`
   font-weight: 400;
   line-height: 170%;
   color: ${({ theme }) => theme.colors.neutral.bg40};
+`;
+
+export const CheckboxWrapper = styled.div`
+  margin-top: 37px;
+  display: flex;
+  height: 40px;
+  box-shadow: 4px 4px 0px 0px rgba(0, 0, 0, 0.1);
+`;
+
+export const CheckboxContainer = styled.div`
+  display: flex;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.neutral.bg05};
+  align-items: center;
+  padding-left: 10px;
+`;
+
+export const Divider = styled.div`
+  width: 10px;
+  background-color: ${({ theme }) => theme.colors.neutral.bg10};
 `;
