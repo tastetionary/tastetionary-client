79f5e68e00be81a6636fba22ff2d734e64be97d3
hovelopin <kimhojin3714@naver.com>
2023-09-24 23:51:36 +0900

    [KB-99] style : grid-layout 스타일 변경

--- src/components/layout/grid-layout/index.tsx
+++ src/components/layout/grid-layout/index.tsx
@@ -1,18 +1,7 @@
-import { styled } from 'styled-components';
-
+import * as S from './page.styled';
 interface Props {
   children: React.ReactNode;
 }
 export default function GridLayout({ children }: Props) {
   return <S.Wrapper>{children}</S.Wrapper>;
 }
-
-const S = {
-  Wrapper: styled.div`
-    padding-top: 100px;
-    height: 100%;
-    background-size: 20px 20px;
-    background-image: ${({ theme }) => `linear-gradient(to right, ${theme.colors.neutral.bg05} 1px, transparent 1px),
-      linear-gradient(to bottom, ${theme.colors.neutral.bg05} 1px, transparent 1px);`};
-  `,
-};
---
+++ src/components/layout/grid-layout/page.styled.ts
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding-top: 100px;
+  height: 100%;
+  background-size: 20px 20px;
+  background-image: ${({ theme }) => `linear-gradient(to right, ${theme.colors.neutral.bg05} 1px, transparent 1px),
+      linear-gradient(to bottom, ${theme.colors.neutral.bg05} 1px, transparent 1px);`};
+`;
