3c944d5cc06335e2ea53a104e44f174fb7c4fa15
hovelopin <kimhojin3714@naver.com>
2023-09-24 23:45:49 +0900

    [KB-99] style : CHeader style 파일 변경

---
+++ src/components/c-header/page.styled.ts
@@ -0,0 +1,41 @@
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 56px;
+  background-color: ${({ theme }) => theme.colors.white};
+  position: fixed;
+  top: 44px;
+  width: 360px;
+  z-index: 1;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+export const BackBtnContainer = styled.div<{ $isBackBtn?: boolean }>`
+  position: absolute;
+  left: 0;
+  cursor: ${({ $isBackBtn }) => ($isBackBtn ? 'pointer' : 'auto')};
+  width: 56px;
+  height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 24px;
+`;
+export const Title = styled.p`
+  margin-left: 8px;
+`;
