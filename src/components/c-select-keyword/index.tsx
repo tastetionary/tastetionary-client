0b666961ed1ef954d9d61d28afe9932b1c22e864
yejiiha <1126yezy@gmail.com>
2023-10-01 14:12:43 +0900

    [KB-117] feat: CSelectKeyword 컴포넌트 추가

---
+++ src/components/c-select-keyword/index.tsx
@@ -0,0 +1,50 @@
+import { Dispatch, SetStateAction } from 'react';
+import * as S from './page.styled';
+
+interface Props {
+  data: { id: number; name: string }[];
+  selectedKeyword: string[];
+  setSelectedKeyword: Dispatch<SetStateAction<string[]>>;
+}
+
+export default function CSelectKeyword({ data, selectedKeyword, setSelectedKeyword }: Props) {
+  return (
+    <S.KeywordContainer>
+      {data?.map((k, i) => {
+        const isSelected = selectedKeyword?.includes(k?.name);
+
+        return (
+          <S.KeywordBtn
+            key={k.id}
+            $isSelected={isSelected}
+            onClick={() => {
+              if (selectedKeyword?.length > 0 && isSelected) {
+                // 이미 선택된 경우
+                if (i === 0) {
+                  setSelectedKeyword([]);
+                } else {
+                  setSelectedKeyword(prev => {
+                    const filtered: string[] = prev.filter(p => p !== k?.name);
+
+                    return [...filtered];
+                  });
+                }
+              } else {
+                // 새롭게 추가하는 경우
+                if (i === 0) {
+                  const allKeywordName = data?.map(k => k.name);
+
+                  setSelectedKeyword(allKeywordName);
+                } else {
+                  setSelectedKeyword(prev => [...prev, k?.name]);
+                }
+              }
+            }}
+          >
+            {k.name}
+          </S.KeywordBtn>
+        );
+      })}
+    </S.KeywordContainer>
+  );
+}
