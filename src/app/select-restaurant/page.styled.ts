47daa5c5063ae6bafdfcbdfb18ed25db2166a999
yejiiha <1126yezy@gmail.com>
2023-10-01 14:14:20 +0900

    [KB-117] feat: select-restaurant 페이지 추가

---
+++ src/app/select-restaurant/page.styled.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const ChangeRegionContainer = styled.div`
+  cursor: pointer;
+  width: 100%;
+  padding: 8px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.neutral.bg05};
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.neutral.bg10};
+  }
+`;
+
+export const FlexBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Region = styled.span`
+  margin-left: 4px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+`;
+
+export const ChangeText = styled(Region)`
+  margin-left: 0;
+  color: ${({ theme }) => theme.colors.neutral.bg40};
+`;
+
+export const SectionContainer = styled.div`
+  margin-bottom: 86px;
+`;
---
+++ src/app/select-restaurant/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import IC_MAP from '@/assets/common/map.svg';
+import restaurant_set from '@/assets/data/restaurant_set.json';
+import MainButton from '@/components/Button/MainButton';
+import RefreshButton from '@/components/Button/RefreshButton';
+import CHeader from '@/components/c-header';
+import CSelectCategory from '@/components/c-select-category';
+import CSelectKeyword from '@/components/c-select-keyword';
+import CSelectSection from '@/components/c-select-section';
+import CSlider from '@/components/c-slider';
+import { useState } from 'react';
+import * as S from './page.styled';
+
+export default function SelectRestaurant() {
+  const categoryData = restaurant_set?.category;
+  const keywordData = restaurant_set?.keyword;
+
+  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+  const [selectedKeyword, setSelectedKeyword] = useState<string[]>([]);
+  const [price, setPrice] = useState<number>(0);
+
+  const btnDisabled = selectedCategory.length === 0 || selectedKeyword.length === 0;
+
+  console.log(price);
+
+  return (
+    <>
+      <CHeader title="식당 고르기" isBackBtn />
+
+      <S.ChangeRegionContainer>
+        <S.FlexBox>
+          <IC_MAP width={24} height={24} />
+
+          <S.Region>논현동</S.Region>
+        </S.FlexBox>
+
+        <S.ChangeText>식사 지역 변경 &gt;</S.ChangeText>
+      </S.ChangeRegionContainer>
+
+      <S.SectionContainer>
+        <CSelectSection title="음식 종류" subtitle="(복수 선택 가능)">
+          <CSelectCategory
+            data={categoryData}
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+        </CSelectSection>
+
+        <CSelectSection title="키워드" subtitle="(복수 선택 가능)">
+          <CSelectKeyword
+            data={keywordData}
+            selectedKeyword={selectedKeyword}
+            setSelectedKeyword={setSelectedKeyword}
+          />
+        </CSelectSection>
+
+        <CSelectSection title="가격">
+          <CSlider changeEvent={value => setPrice(value)} />
+        </CSelectSection>
+      </S.SectionContainer>
+
+      <MainButton btnText="추첨 시작" disabled={btnDisabled} style={{ maxWidth: 240, margin: '48px auto 0' }} />
+
+      <RefreshButton
+        btnText="선택 초기화"
+        disabled={btnDisabled}
+        onClick={() => {
+          setSelectedCategory([]);
+          setSelectedKeyword([]);
+        }}
+      />
+    </>
+  );
+}
