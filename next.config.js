b1ddfacff602fdd5bb35544fbc4ae0274cc528db
yejiiha <1126yezy@gmail.com>
2023-10-02 13:55:54 +0900

    [KB-119] feat: 네이버 이미지 검색 api 사용 위해 서버 주소 우회

--- next.config.js
+++ next.config.js
@@ -1,20 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true,
-    experimental: {
-        appDir: true,
-    },
-    compiler: {
-        styledComponents: true,
-    },
-    webpack: config => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
-        });
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-        return config;
-    },
+    return config;
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_DEV_SERVER_URL,
+      },
+      {
+        source: '/search-image-api:path*',
+        destination: process.env.NEXT_PUBLIC_SEARCH_IMAGE_SERVER_URL,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
