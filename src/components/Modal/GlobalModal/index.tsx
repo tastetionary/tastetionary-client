7cb8cc3c16e280b288a0054276d35348f7ddafb1
yejiiha <1126yezy@gmail.com>
2023-10-08 20:02:42 +0900

    [KB-135] feat: 로딩 모달 추가

--- src/components/Modal/GlobalModal/index.tsx
+++ src/components/Modal/GlobalModal/index.tsx
@@ -4,11 +4,13 @@ import useModal from './hooks/useModal';
 
 const ExampleModal = loadable(() => import('../ExampleModal'), { ssr: false });
 const DialogModal = loadable(() => import('../DialogModal'), { ssr: false });
+const LoadingModal = loadable(() => import('../LoadingModal'), { ssr: false });
 
 // 사용할 모달 컴포넌트들을 담은 Object
 export const MODAL_TYPES = {
   example: ExampleModal as FunctionComponent<ComponentProps<typeof ExampleModal>>,
   dialog: DialogModal as FunctionComponent<ComponentProps<typeof DialogModal>>,
+  loading: LoadingModal as FunctionComponent<ComponentProps<typeof LoadingModal>>,
 };
 
 export default function GlobalModal() {
