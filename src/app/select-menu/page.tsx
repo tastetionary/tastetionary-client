736544b98655cdb871575ab06718a24c9f945320
yejiiha <1126yezy@gmail.com>
2023-10-11 23:57:58 +0900

    [KB-151] feat: 메뉴 추첨, 결과 페이지 서버 기능 반영

--- src/app/select-menu/page.tsx
+++ src/app/select-menu/page.tsx
@@ -1,26 +1,26 @@
 'use client';
 
-import menu_set from '@/assets/data/menu_set.json';
-import MainButton from '@/components/Button/MainButton';
+import { getFoodOption } from '@/apis/food/option';
 import RefreshButton from '@/components/Button/RefreshButton';
 import CHeader from '@/components/c-header';
+import CSelectButton from '@/components/c-select-button';
 import CSelectCategory from '@/components/c-select-category';
 import CSelectKeyword from '@/components/c-select-keyword';
 import CSelectSection from '@/components/c-select-section';
-import { useState } from 'react';
+import { selectFoodState } from '@/lib/atom';
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
 import * as S from './page.styled';
 
 export default function SelectMenu() {
-  const menuData = menu_set?.category;
-  const keywordData = menu_set?.keyword;
+  const foodState = useRecoilValue(selectFoodState);
 
-  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
-  const [selectedKeyword, setSelectedKeyword] = useState<string[]>([]);
+  const btnDisabled = foodState?.category?.length === 0 || foodState?.keyword?.length === 0;
 
-  const btnDisabled = selectedCategory.length === 0 || selectedKeyword.length === 0;
-
-  console.log(selectedCategory);
-  console.log(selectedKeyword);
+  const { data } = useQuery(['food-option'], () => getFoodOption(), {
+    cacheTime: 0,
+    staleTime: 0,
+  });
 
   return (
     <>
@@ -28,32 +28,22 @@ export default function SelectMenu() {
 
       <S.Container>
         <CSelectSection title="음식 종류 선택" subtitle="(복수 선택 가능)">
-          <CSelectCategory
-            data={menuData}
-            selectedCategory={selectedCategory}
-            setSelectedCategory={setSelectedCategory}
-          />
+          <CSelectCategory data={data?.categories} selectType="food" />
         </CSelectSection>
 
-        <CSelectSection title="키워드" subtitle="복수 선택 가능">
-          <CSelectKeyword
-            data={keywordData}
-            selectedKeyword={selectedKeyword}
-            setSelectedKeyword={setSelectedKeyword}
-          />
+        <CSelectSection title="키워드" subtitle="(복수 선택 가능)">
+          <CSelectKeyword data={data?.keywords} selectType="food" />
         </CSelectSection>
       </S.Container>
 
-      <MainButton btnText="메뉴 추첨 시작" disabled={btnDisabled} style={{ maxWidth: 240, margin: '48px auto 0' }} />
-
-      <RefreshButton
-        btnText="선택 초기화"
+      <CSelectButton
+        btnText="메뉴 추첨 시작"
+        selectType="food"
         disabled={btnDisabled}
-        onClick={() => {
-          setSelectedCategory([]);
-          setSelectedKeyword([]);
-        }}
+        style={{ maxWidth: 240, margin: '48px auto 0' }}
       />
+
+      <RefreshButton btnText="선택 초기화" disabled={btnDisabled} onClick={() => {}} />
     </>
   );
 }
--- src/app/select-menu/result/page.tsx
+++ src/app/select-menu/result/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 
-import MainButton from '@/components/Button/MainButton';
 import RefreshButton from '@/components/Button/RefreshButton';
 import CHeader from '@/components/c-header';
+import CSelectButton from '@/components/c-select-button';
 import CStickyMemo from '@/components/c-sticky-memo';
+import { selectResultState } from '@/lib/atom';
 import { useRouter } from 'next/navigation';
+import { useRecoilValue } from 'recoil';
 import MENU_KOREAN from '../../../../public/image/Menu/menu_korean.svg';
 import * as S from './page.styled';
 
 export default function SelectMenuResult() {
   const router = useRouter();
+
+  const food = useRecoilValue(selectResultState)?.food?.name;
 
   return (
     <>
@@ -19,13 +23,13 @@ export default function SelectMenuResult() {
         <S.ResultTitle>오늘의 점심 메뉴는...</S.ResultTitle>
 
         <CStickyMemo>
-          <S.MemoTitle>김치찌개!</S.MemoTitle>
+          <S.MemoTitle>{food ? `${food}!` : ''}</S.MemoTitle>
 
           <MENU_KOREAN width={160} height={160} />
         </CStickyMemo>
 
         <S.ButtonContainer>
-          <MainButton btnText="다시 선택하기" />
+          <CSelectButton btnText="다시 선택하기" selectType="food" />
 
           <RefreshButton btnText="조건 재설정" onClick={() => router.push('/select-menu')} />
         </S.ButtonContainer>
