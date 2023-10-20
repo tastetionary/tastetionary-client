514c180ff41106d2ddf93406c742cff335ed6fe2
yejiiha <1126yezy@gmail.com>
2023-10-20 22:35:35 +0900

    [KB-164] fix: 하단 탭바 메뉴 Active 상태에서 border-top 삭제

--- src/components/c-nav-button/page.styled.ts
+++ src/components/c-nav-button/page.styled.ts
@@ -4,7 +4,7 @@ export const Button = styled.button<{ isActive: Boolean }>`
   width: 120px;
   height: 60px;
   background-color: ${({ theme, isActive }) => (isActive ? 'white' : theme.colors.neutral.bg05)};
-  border: 1px solid #ced9db;
+  border: ${({ isActive }) => (isActive ? 0 : 2)}px solid #ced9db;
 `;
 export const Icon = styled.div``;
 
