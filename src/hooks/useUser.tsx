1f5364278fe8c74f1784f0de7e57c06cf4aed91c
yejiiha <1126yezy@gmail.com>
2023-10-22 20:52:23 +0900

    [KB-174] sessionStorage 조건 수정

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -2,20 +2,19 @@ import { UserRes } from '@/apis/user/getUser';
 import { queryClient } from '@/lib/react-query/ReactQueryProvider';
 
 export default function useUser() {
-  if (typeof window === undefined) {
+  if (typeof window === 'undefined')
     return {
       isLoggedIn: false,
     };
-  }
 
-  const token = (sessionStorage as Storage).getItem('token') as string | undefined;
+  const token = typeof window !== 'undefined' ? ((sessionStorage as Storage).getItem('token') as string) : undefined;
 
   const userData = queryClient.getQueryData(['user']) as UserRes;
 
   return {
     isLoggedIn: Boolean(token),
     token,
-    hasActivityArea: userData ? Boolean(userData?.activity_area?.address) : false,
+    hasActivityArea: userData && token ? Boolean(userData?.activity_area?.address) : false,
     data: {
       ...userData,
     },
