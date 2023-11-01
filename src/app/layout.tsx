eb199238cb37bbb4eb663056f2b4ac48f9072680
yejiiha <1126yezy@gmail.com>
2023-11-01 21:23:50 +0900

    [KB-202] <link /> 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <title>맛셔너리</title>
         <meta name="description" content="직장인 점심 메뉴 & 식당 추첨 서비스 (우리회사 근처 맛집 나무위키)" />
-        <link rel="icon" type="image/png" href="%PUBLIC_URL%/favivon/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="%PUBLIC_URL%/favivon/favicon-16x16.png" sizes="16x16" />
+        {/* <link rel="icon" type="image/png" href="%PUBLIC_URL%/favivon/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="%PUBLIC_URL%/favivon/favicon-16x16.png" sizes="16x16" /> */}
       </head>
       <body className={`${subFont.variable} ${mainFont.variable}`}>
         <StyledComponentsRegistry>
