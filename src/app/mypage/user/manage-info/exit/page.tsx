d6f91fb823559ffb539f7484cfede4bf8ca70f00
yejiiha <1126yezy@gmail.com>
2023-12-03 18:35:44 +0900

    fix: CheckBox2 사용 삭제

--- src/app/mypage/user/manage-info/exit/page.tsx
+++ src/app/mypage/user/manage-info/exit/page.tsx
@@ -2,19 +2,21 @@
 
 import { CheckboxContainer, Divider } from '@/app/sign-up/components/terms/page.styled';
 import MainButton from '@/components/Button/MainButton';
-import CheckBox2 from '@/components/CheckBox/CheckBox2';
 import CHeader from '@/components/c-header';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import * as S from './page.styled';
 
 export default function MyPageUserExit() {
+  const { push } = useRouter();
+
   const reasonList = [
     '사용이 불편해요.',
     '더 이상 서비스를 이용하지 않아요.',
-    '사용 빈도가 낮아요',
-    '더 마음에 드는 서비스를 찾았어요',
-    '컨텐츠의 신뢰도가 떨어져요',
-    '개인정보를 삭제하고 싶어요',
+    '사용 빈도가 낮아요.',
+    '더 마음에 드는 서비스를 찾았어요.',
+    '컨텐츠의 신뢰도가 떨어져요.',
+    '개인정보를 삭제하고 싶어요.',
   ];
 
   const defaultReasonState = reasonList.reduce(
@@ -50,12 +52,14 @@ export default function MyPageUserExit() {
             <S.ReasonItem key={i}>
               <Divider isActive={reason[i]}></Divider>
               <CheckboxContainer isActive={reason[i]}>
-                <CheckBox2
-                  checkBoxId={`reason-${i}`}
-                  onChangeEvent={checked => onReasonClick(i, checked)}
-                  checked={reason[i]}
-                  label={r}
-                />
+                <S.ReasonCheckBoxContainer>
+                  <S.ReasonCheckBox
+                    id={`reason-${i}`}
+                    checked={reason[i]}
+                    onChange={({ target: { checked } }) => onReasonClick(i, checked)}
+                  />
+                  <S.ReasonLabel htmlFor={`reason-${i}`}>{r}</S.ReasonLabel>
+                </S.ReasonCheckBoxContainer>
               </CheckboxContainer>
             </S.ReasonItem>
           ))}
@@ -64,6 +68,7 @@ export default function MyPageUserExit() {
         <MainButton
           btnText="탈퇴하기"
           disabled={!(Object.values(reason).filter((r: boolean) => r === true)?.length > 0)}
+          onClick={() => push('/mypage/user/manage-info/exit/success')}
         />
       </S.Container>
     </>
