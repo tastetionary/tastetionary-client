c194cd7498f24bd04a6fee7ab44b2b04806d5385
yejiiha <1126yezy@gmail.com>
2023-12-03 18:36:05 +0900

    feat: 회원 탈퇴 완료 페이지 추가

---
+++ src/app/mypage/user/manage-info/exit/success/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import * as S from '@/app/sign-up/components/complete/page.styled';
+import COMPLETE from '@/assets/logo/complete.svg';
+import MainButton from '@/components/Button/MainButton';
+import CHeader from '@/components/c-header';
+import { useRouter } from 'next/navigation';
+
+export default function MyPageUserExitSuccess() {
+  const { push } = useRouter();
+
+  return (
+    <>
+      <CHeader title="회원 탈퇴 완료" />
+
+      <S.Wrapper>
+        <COMPLETE />
+
+        <S.Title>회원 탈퇴가 완료되었습니다.</S.Title>
+        <S.SubTitle>
+          지금까지 맛셔너리를 이용해주셔서 감사합니다. <br />더 나은 서비스를 위해 노력하겠습니다.
+        </S.SubTitle>
+
+        <MainButton type="button" btnText="홈 화면으로" onClick={() => push('/')} />
+      </S.Wrapper>
+    </>
+  );
+}
