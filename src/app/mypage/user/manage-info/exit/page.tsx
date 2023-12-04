973f4f22245fda1ba12528aee4e6efe6d96f0349
yejiiha <1126yezy@gmail.com>
2023-12-04 20:17:45 +0900

    fix: 회원 탈퇴 버튼 조건 분리

--- src/app/mypage/user/manage-info/exit/page.tsx
+++ src/app/mypage/user/manage-info/exit/page.tsx
@@ -4,6 +4,7 @@ import { CheckboxContainer, Divider } from '@/app/sign-up/components/terms/page.
 import MainButton from '@/components/Button/MainButton';
 import CheckBox2 from '@/components/CheckBox/CheckBox2';
 import CHeader from '@/components/c-header';
+import { userExitReasonList } from '@/constants/user-exit';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import * as S from './page.styled';
@@ -11,16 +12,7 @@ import * as S from './page.styled';
 export default function MyPageUserExit() {
   const { push } = useRouter();
 
-  const reasonList = [
-    '사용이 불편해요.',
-    '더 이상 서비스를 이용하지 않아요.',
-    '사용 빈도가 낮아요.',
-    '더 마음에 드는 서비스를 찾았어요.',
-    '컨텐츠의 신뢰도가 떨어져요.',
-    '개인정보를 삭제하고 싶어요.',
-  ];
-
-  const defaultReasonState = reasonList.reduce(
+  const defaultReasonState = userExitReasonList.reduce(
     (acc, _, index) => ({
       ...acc,
       [index]: false,
@@ -37,6 +29,8 @@ export default function MyPageUserExit() {
     });
   };
 
+  const exitBtnDisabled = Object.values(reason).filter((r: boolean) => r === true)?.length > 0 ? false : true;
+
   return (
     <>
       <CHeader title="회원 탈퇴" isBackBtn />
@@ -49,7 +43,7 @@ export default function MyPageUserExit() {
         </S.Title>
 
         <S.ReasonList>
-          {reasonList?.map((r, i) => (
+          {userExitReasonList?.map((r, i) => (
             <S.ReasonItem key={i}>
               <Divider isActive={reason[i]}></Divider>
               <CheckboxContainer isActive={reason[i]}>
@@ -66,7 +60,7 @@ export default function MyPageUserExit() {
 
         <MainButton
           btnText="탈퇴하기"
-          disabled={!(Object.values(reason).filter((r: boolean) => r === true)?.length > 0)}
+          disabled={exitBtnDisabled}
           onClick={() => push('/mypage/user/manage-info/exit/success')}
         />
       </S.Container>
