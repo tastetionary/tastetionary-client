71e365fb240c8afa395de4d2e4a1c259f0bf0bdd
yejiiha <1126yezy@gmail.com>
2023-12-07 22:30:11 +0900

    feat: 마이페이지 메인 화면 (비로그인) 추가

--- src/app/mypage/page.styled.ts
+++ src/app/mypage/page.styled.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
 export const MenuList = styled.div``;
+
+export const NotLogInContainer = styled.div`
+  padding: 40px 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.bg10};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NotLoginText = styled.span`
+  cursor: pointer;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+`;
