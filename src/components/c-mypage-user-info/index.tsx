f3404d48aa41e3a16944f19df2f397151d9704f2
hovelopin <kimhojin3714@naver.com>
2023-12-18 21:26:15 +0900

    [TAS-109] fix : style 수정정

--- src/components/c-mypage-user-info/index.tsx
+++ src/components/c-mypage-user-info/index.tsx
@@ -60,17 +60,17 @@ export default function CMyPageUserInfo() {
 
       <S.AreaContainer>
         <S.AreaBox>
-          <S.AreaBoxLValue onClick={() => router.push('mypage/region-setting?category=dining_area')}>
+          <S.AreaBoxLabel onClick={() => router.push('mypage/region-setting?category=dining_area')}>
             식사 지역
-          </S.AreaBoxLValue>
+          </S.AreaBoxLabel>
 
           <S.AreaBoxLValue>{getShortAddress(data?.dining_area?.address)}</S.AreaBoxLValue>
         </S.AreaBox>
 
         <S.AreaBox>
-          <S.AreaBoxLValue onClick={() => router.push('mypage/region-setting?category=activity_area')}>
+          <S.AreaBoxLabel onClick={() => router.push('mypage/region-setting?category=activity_area')}>
             활동 지역
-          </S.AreaBoxLValue>
+          </S.AreaBoxLabel>
 
           <S.AreaBoxLValue>{getShortAddress(data?.activity_area?.address)}</S.AreaBoxLValue>
         </S.AreaBox>
