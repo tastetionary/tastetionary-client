9d583476cd0e57f449b59c142dec36e4856db6e6
hovelopin <kimhojin3714@naver.com>
2024-01-02 20:58:53 +0900

    [TAS-61] feat : 개인정보처리방침 , 서비스 이용약관 라우팅 처리

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -45,7 +45,13 @@ export default function MyPage() {
             items={[{ name: '공지사항' }, { name: '자주 묻는 질문' }, { name: '의견 보내기' }]}
           />
 
-          <CMypageMenu title="약관 및 정책" items={[{ name: '서비스 이용약관' }, { name: '개인정보 처리 방침' }]} />
+          <CMypageMenu
+            title="약관 및 정책"
+            items={[
+              { name: '서비스 이용약관', pathname: '/sign-up?step=terms-of-service' },
+              { name: '개인정보 처리 방침', pathname: '/sign-up?step=privacy-notice' },
+            ]}
+          />
         </S.MenuList>
       </GNBLayout>
     </>
