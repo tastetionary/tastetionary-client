0f1cb484f7dbd1de67c14c0dc84b68d4fc5307e5
hovelopin <kimhojin3714@naver.com>
2024-01-04 21:08:24 +0900

    [TAS-124] fix : hydration error 및 console 에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </head>
-      <body className={`${subFont.variable} ${mainFont.variable}`}>
+      <body className={`${subFont.variable} ${mainFont.variable}`} suppressHydrationWarning>
         <StyledComponentsRegistry>
           <RecoilRootWrapper>
             <StyledComponentsWrapper>{children}</StyledComponentsWrapper>
