af43bb7eefc75f99997e2e2d546e53eab54608df
yejiiha <1126yezy@gmail.com>
2024-05-11 21:24:30 +0900

    feat: CHeader Tailwind CSS 적용

--- src/components/c-header/index.tsx
+++ src/components/c-header/index.tsx
@@ -1,5 +1,6 @@
 import ISBACK_BTN from '@/assets/logo/back_btn.svg';
 import MAIN_LOGO from '@/assets/logo/main_logo.svg';
+import { cn } from '@/utils/styles.utils';
 import { useRouter } from 'next/navigation';
 import * as S from './page.styled';
 
@@ -12,26 +13,27 @@ interface Props {
 export default function CHeader({ isLogo = false, isBackBtn = false, title }: Props) {
   const router = useRouter();
 
+  const backBtnState = isBackBtn ? 'isBackBtn' : 'default';
+  const titleState = isLogo ? 'isLogo' : 'default';
+
   return (
-    <>
-      <S.Wrapper>
-        <S.Container>
-          <S.BackBtnContainer $isBackBtn={isBackBtn}>
-            {isBackBtn && <ISBACK_BTN onClick={() => router.back()} />}
-          </S.BackBtnContainer>
-          <S.TitleContainer
-            $isLogo={isLogo}
-            onClick={() => {
-              if (isLogo) {
-                router.push('/');
-              }
-            }}
-          >
-            {isLogo && <MAIN_LOGO />}
-            <S.Title>{title}</S.Title>
-          </S.TitleContainer>
-        </S.Container>
-      </S.Wrapper>
-    </>
+    <div className="fixed top-44 z-1 h-56 w-360 bg-white mobile:w-full">
+      <div className="relative flex h-full items-center justify-center">
+        <div className={cn(S.backBtnVariants({ cursor: backBtnState }))}>
+          {isBackBtn && <ISBACK_BTN onClick={() => router.back()} />}
+        </div>
+        <div
+          className={cn(S.titleVariants({ cursor: titleState }))}
+          onClick={() => {
+            if (isLogo) {
+              router.push('/');
+            }
+          }}
+        >
+          {isLogo && <MAIN_LOGO />}
+          <p className="ml-8">{title}</p>
+        </div>
+      </div>
+    </div>
   );
 }
