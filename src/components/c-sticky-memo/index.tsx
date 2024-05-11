9fbda3247ab3aedc5633f7199d01bcf2b12c8d8d
yejiiha <1126yezy@gmail.com>
2024-05-11 20:58:38 +0900

    feat: CStickyMemo Tailwind CSS 적용

--- src/components/c-sticky-memo/index.tsx
+++ src/components/c-sticky-memo/index.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import * as S from './page.styled';
 
 interface CStickyMemoProps {
   children: ReactNode;
@@ -7,10 +6,10 @@ interface CStickyMemoProps {
 
 export default function CStickyMemo({ children }: CStickyMemoProps) {
   return (
-    <S.Container>
-      <S.Shadow />
-      <S.StickyMemoHeader />
-      <S.StickyMemo>{children}</S.StickyMemo>
-    </S.Container>
+    <div className="relative z-[0] h-full w-full">
+      <div className="absolute left-4 top-4 z-[-1] h-full w-full bg-black bg-opacity-10" />
+      <div className="h-20 w-full bg-primary-y20" />
+      <div className="flex w-full flex-col items-center bg-primary-y10 p-24">{children}</div>
+    </div>
   );
 }
