864e72b309ba482d1957d3c8b9d9937908c0c36e
hovelopin <kimhojin3714@naver.com>
2024-06-23 22:20:28 +0900

    [TAS-229] feat : rc-progress 스타일 수정

--- src/app/select-restaurant/result/review/[restaurantId]/_components/revisiting-progressbar/index.tsx
+++ src/app/select-restaurant/result/review/[restaurantId]/_components/revisiting-progressbar/index.tsx
@@ -17,6 +17,12 @@ export default function RevisitingProgressbar({ mock }: any) {
         <span className="absolute left-10 top-10 font-normal text-primary-y80">{mock.category}</span>
         <span className="absolute right-10 top-10">{mock.count}</span>
       </div>
+      <div
+        style={{
+          width: `${mock.percent + 0.5}%`,
+        }}
+        className="z-1000 absolute top-0 h-32 border-1 border-solid border-primary-y50"
+      ></div>
     </div>
   );
 }
