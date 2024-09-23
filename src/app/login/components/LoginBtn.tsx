1b70d096171da3253c32f0cda98d3bce37b00917
yejiiha <1126yezy@gmail.com>
2024-09-23 23:49:08 +0900

    feat: LoginBtn 컴포넌트 만들어서 로그인 버튼 UI 관리

---
+++ src/app/login/components/LoginBtn.tsx
@@ -0,0 +1,50 @@
+import IC_EMAIL from '@/assets/common/Icons/email.svg';
+import LOGO_APPLE from '@/assets/logo/sns/logo_apple.svg';
+import LOGO_GOOGLE from '@/assets/logo/sns/logo_google.svg';
+import LOGO_KAKAO from '@/assets/logo/sns/logo_kakao.svg';
+import LOGO_NAVER from '@/assets/logo/sns/logo_naver.svg';
+import clsx from 'clsx';
+import { ButtonHTMLAttributes, Fragment } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  loginType: 'kakao' | 'naver' | 'google' | 'apple' | 'email';
+}
+
+export default function LoginBtn({ loginType, ...rest }: Props) {
+  const loginButtonDefaultStyle = (bgColor: string) => {
+    return `default-btn bg-${bgColor}`;
+  };
+
+  if (loginType === 'naver')
+    return (
+      <Fragment>
+        <button className={clsx(`${loginButtonDefaultStyle('[#5AC467]')} h-48 w-48`)} {...rest}>
+          <LOGO_NAVER width={24} height={24} />
+        </button>
+
+        <div id="naverIdLogin" style={{ display: 'none' }} />
+      </Fragment>
+    );
+
+  return (
+    <button
+      className={
+        loginType === 'kakao'
+          ? `default-btn w-full gap-xs bg-[#fae64d] py-sm`
+          : clsx(`${loginButtonDefaultStyle(loginType === 'apple' ? 'black' : 'white')} h-48 w-48`)
+      }
+      {...rest}
+    >
+      {loginType === 'email' ? (
+        <IC_EMAIL width={24} height={24} />
+      ) : loginType === 'google' ? (
+        <LOGO_GOOGLE width={24} height={24} />
+      ) : loginType === 'apple' ? (
+        <LOGO_APPLE width={24} height={24} />
+      ) : (
+        <LOGO_KAKAO width={20} height={20} />
+      )}
+      {loginType === 'kakao' && <span className="body1">카카오 로그인</span>}
+    </button>
+  );
+}
