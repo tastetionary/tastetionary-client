e1d36d7ab1159d6ed784bb66d1f9c72ae9bff588
yejiiha <1126yezy@gmail.com>
2024-09-30 00:03:58 +0900

    feat: orange 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -141,6 +141,18 @@ module.exports = {
         r10: '#FFCDD1',
         r05: '#FFEBEE',
       },
+      orange: {
+        o90: '#C13001',
+        o80: '#D94000',
+        o70: '#E64801',
+        o60: '#F44E00',
+        o50: '#FF5601',
+        o40: '#FF6E32',
+        o30: '#FE895A',
+        o20: '#FFAA8A',
+        o10: '#FFCBB8',
+        o05: '#FCE9E6',
+      },
     },
   },
   plugins: [
