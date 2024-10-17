59a8ea84f7ff978152ec4eb4e6611f09a4d96158
yejiiha <1126yezy@gmail.com>
2024-10-18 00:05:22 +0900

    fix: 로그인 API 수정 반영

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -1,23 +1,25 @@
 import http from '../http';
 
 interface PostLoginParams {
-  identification: string;
-  password: string;
-  category: 'email';
+  identification?: string;
+  password?: string;
+  category: 'email' | 'kakao' | 'naver' | 'google';
+  code?: string;
 }
 
 interface AuthRepository {
-  postLogin: ({ identification, password, category }: PostLoginParams) => Promise<any>;
+  postLogin: ({ identification, password, category, code }: PostLoginParams) => Promise<any>;
   postLogout: ({ token }: { token: string }) => Promise<any>;
 }
 
 const authRepository = (): AuthRepository => {
   return {
-    postLogin: async ({ identification, password, category }) =>
+    postLogin: async ({ identification, password, category, code }) =>
       await http.post<any, PostLoginParams>('/apis/v1/account/tokens', {
         identification,
         password,
         category,
+        code,
       }),
     postLogout: async ({ token }) =>
       await http.delete('/apis/v1/account/tokens', {
