e6acc68b7e91d497097dfb9bf17e8e8138df7c0a
hovelopin <kimhojin3714@naver.com>
2024-10-21 00:03:18 +0900

    fix: 마이페이지 UI 변경작업 완료

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,16 +2,32 @@
 
 import CHeader from '@/components/c-header';
 import CMypageMenu from '@/components/c-mypage-menu';
-import CMyPageUserInfo from '@/components/c-mypage-user-info';
 import GNBLayout from '@/components/layout/gnb-layout';
+import { MODAL_TYPES } from '@/components/Modal/GlobalModal';
+import useModal from '@/components/Modal/GlobalModal/hooks/useModal';
 import useUser from '@/hooks/useUser';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import useLogoutMutate from '../login/hooks/useLogoutMutate';
 import * as S from './page.styled';
 
 export default function MyPage() {
   const { push } = useRouter();
-  const { isLoggedIn } = useUser();
+  const { isLoggedIn, token, data } = useUser();
+  const { openModal, closeModal } = useModal();
+  const { mutate: logoutMutate } = useLogoutMutate();
+
+  const logoutModal = () => {
+    openModal(MODAL_TYPES.dialog, {
+      title: '로그아웃 하시겠습니까?',
+      cancelText: '취소',
+      needClose: true,
+      handleClose: () => closeModal(MODAL_TYPES.dialog),
+      handleConfirm: () => {
+        if (token) logoutMutate({ token });
+      },
+    });
+  };
 
   return (
     <>
@@ -19,7 +35,16 @@ export default function MyPage() {
 
       <GNBLayout>
         {isLoggedIn ? (
-          <CMyPageUserInfo />
+          // 호진FIXME: 해당 부분 컴포넌트화 가능한 로직!
+          <S.NotLogInContainer>
+            <div>
+              <p className="flex cursor-pointer !font-pretendard text-xl font-bold" onClick={() => push('/login')}>
+                {data?.nickname}
+                <Image src={'/image/Mypage/arrow_right.svg'} width={24} height={24} alt="arrow_right" />
+              </p>
+              <p className="mt-3 !font-pretendard text-sm">{data?.account?.accountEmail}</p>
+            </div>
+          </S.NotLogInContainer>
         ) : (
           <S.NotLogInContainer>
             <div>
@@ -35,9 +60,9 @@ export default function MyPage() {
           {isLoggedIn && (
             <CMypageMenu
               items={[
-                { name: '개인정보 관리', hasArrow: true, clickEvent: () => push('/mypage/user/manage-info') },
-                { name: '내 리뷰 관리' },
-                { name: '북마크 목록' },
+                { name: '개인정보 관리', clickEvent: () => push('/mypage/user/manage-info') },
+                { name: '작성한 리뷰 관리' },
+                { name: '북마크 식당 관리' },
                 { name: '추천 제외 식당 보기' },
               ]}
             />
@@ -63,6 +88,8 @@ export default function MyPage() {
               { name: '개인정보 처리 방침', clickEvent: () => push('/sign-up?step=privacy-notice') },
             ]}
           />
+
+          <CMypageMenu items={[{ name: '로그아웃', clickEvent: () => logoutModal() }]} />
         </S.MenuList>
       </GNBLayout>
     </>
