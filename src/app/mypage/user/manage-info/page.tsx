69ddb83956d7a518d9dfe3bbf918ad860cd24678
hovelopin <kimhojin3714@naver.com>
2024-11-18 20:35:46 +0900

    feat : 닉네임 수정 페이지 작업 완료

--- src/app/mypage/user/manage-info/page.tsx
+++ src/app/mypage/user/manage-info/page.tsx
@@ -10,7 +10,7 @@ export default function MyPageUserManageInfo() {
   return (
     <>
       <CHeader title="개인정보 관리" isBackBtn />
-      <CMypageItem title="닉네임 수정" />
+      <CMypageItem title="닉네임 수정" onClick={() => push('/mypage/user/manage-info/nickname')} />
       <CMypageItem title="비밀번호 재설정" />
       <CMypageItem title="지역 변경" />
       <CMypageItem title="회원 탈퇴" onClick={() => push('/mypage/user/manage-info/exit')} />
