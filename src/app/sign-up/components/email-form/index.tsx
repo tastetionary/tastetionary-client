b2d85a2d88501d8167aaacafee4cb412f9b21e57
yejiiha <1126yezy@gmail.com>
2024-11-28 16:07:45 +0900

    fix: 상단 여백 수정 및 회원가입 단계 title, desc, 버튼 영역 스타일 값 수정

--- src/app/sign-up/components/email-form/index.tsx
+++ src/app/sign-up/components/email-form/index.tsx
@@ -1,5 +1,6 @@
 import DefaultButton from '@/components/Button/DefaultButton';
 // import Header from '@/components/Header';
+import BottomButtonContainer from '@/components/Button/BottomButtonContainer';
 import CHeader from '@/components/c-header';
 import TextInput from '@/components/Input/TextInput';
 import { emailRegex } from '@/constants';
@@ -31,12 +32,12 @@ export default function EmailForm({ onNext, setEmailAuthId }: Props) {
   return (
     <>
       <CHeader title="회원가입" />
-      <div className="mx-8 my-20">
+      <div className="mx-xl mb-20 mt-xl">
         <header>
-          <h1 className="!font-pretendard text-xl font-bold leading-8">
+          <h1 className="title2 font-bold">
             아이디로 사용할 <br /> 이메일 주소를 입력해 주세요. ✍️
           </h1>
-          <p className="mt-3 !font-pretendard text-sm leading-5 text-neutral-bg80">
+          <p className="body2 mt-3 text-neutral-bg80">
             입력하신 이메일 주소는 로그인과 비밀번호 재설정, 중요한 소식 전달에 사용됩니다. 정확한 이메일 주소를 입력해
             주세요.
           </p>
@@ -53,7 +54,7 @@ export default function EmailForm({ onNext, setEmailAuthId }: Props) {
         </section>
       </div>
 
-      <footer className="fixed bottom-[30px] w-[360px] px-25 pb-10 pt-5 mobile:w-full">
+      <BottomButtonContainer>
         <DefaultButton
           bgColor="yellow"
           customStyle="flex w-full py-[12px] px-[16px]"
@@ -63,7 +64,7 @@ export default function EmailForm({ onNext, setEmailAuthId }: Props) {
         >
           <span className="font-pretendard text-white">다음</span>
         </DefaultButton>
-      </footer>
+      </BottomButtonContainer>
     </>
   );
 }
--- src/app/sign-up/components/user-info-form/index.tsx
+++ src/app/sign-up/components/user-info-form/index.tsx
@@ -1,3 +1,4 @@
+import BottomButtonContainer from '@/components/Button/BottomButtonContainer';
 import DefaultButton from '@/components/Button/DefaultButton';
 import CHeader from '@/components/c-header';
 import TextInput from '@/components/Input/TextInput';
@@ -25,6 +26,7 @@ export default function UserInfoForm({ onNext }: Props) {
   }>();
 
   const accountEmail = getValues('account.identification');
+  const nickname = getValues('nickname');
 
   // 이메일 주소와 , emailAuthId값을 전달받아서 요청에 보내야함!
   const { mutate: accountAuthCodeMutate } = useAccountAuthCodeMutate({
@@ -40,20 +42,20 @@ export default function UserInfoForm({ onNext }: Props) {
   };
 
   const handleValidateNickname = () => {
-    validateNicknameMutate({ nickname: getValues('nickname') });
+    validateNicknameMutate({ nickname });
   };
 
   return (
     <>
       <CHeader title="회원가입" />
 
-      <div className="mx-8 my-20">
+      <div className="mx-xl mb-40 mt-xl">
         <header>
-          <h1 className="!font-pretendard text-xl font-bold leading-8">
+          <h1 className="title2 font-bold">
             원활한 서비스 이용을 위해 <br />
             아래 회원 정보를 입력해 주세요. ✍️
           </h1>
-          <p className="mt-3 !font-pretendard leading-5 text-neutral-bg80">
+          <p className="body2 mt-3 text-neutral-bg80">
             회원 정보는 개인화된 추천과 원활한 서비스 제공을 위해 사용되며, <br /> 안전하게 보호됩니다.
           </p>
         </header>
@@ -96,6 +98,7 @@ export default function UserInfoForm({ onNext }: Props) {
               bgColor="yellow"
               customStyle="bottom-0 absolute h-48 right-0 px-[16px] py-[12px] text-xs"
               onClick={handleValidateNickname}
+              disabled={nickname.length === 0}
               type="button"
             >
               <span className="!font-pretendard text-white">중복 확인</span>
@@ -103,62 +106,26 @@ export default function UserInfoForm({ onNext }: Props) {
           </div>
         </section>
       </div>
-      <footer className="fixed bottom-[30px] w-[360px] px-25 pb-10 pt-5 mobile:w-full">
-        <div className="flex justify-center gap-2">
-          <p className="!font-pretendard text-sm text-neutral-bg80">인증 코드를 받지 못하셨나요?</p>
-          <DefaultButton bgColor="gray" customStyle="px-[12px] py-[4px]" onClick={onEmailAuthRequest}>
-            <span className="!font-pretendard">메일 재전송</span>
+
+      <BottomButtonContainer>
+        <footer className="w-full">
+          <div className="flex justify-center gap-2">
+            <p className="!font-pretendard text-sm text-neutral-bg80">인증 코드를 받지 못하셨나요?</p>
+            <DefaultButton bgColor="gray" customStyle="px-[12px] py-[4px]" onClick={onEmailAuthRequest}>
+              <span className="!font-pretendard">메일 재전송</span>
+            </DefaultButton>
+          </div>
+          <DefaultButton
+            bgColor="yellow"
+            customStyle="flex w-full py-[12px] px-[16px] mt-6"
+            disabled={!isDirty || !isValid}
+            onClick={onNext}
+            type="button"
+          >
+            <span className="!font-pretendard text-white">다음</span>
           </DefaultButton>
-        </div>
-        <DefaultButton
-          bgColor="yellow"
-          customStyle="flex w-full py-[12px] px-[16px] mt-6"
-          disabled={!isDirty || !isValid}
-          onClick={onNext}
-          type="button"
-        >
-          <span className="!font-pretendard text-white">다음</span>
-        </DefaultButton>
-      </footer>
-
-      {/* <S.Wrapper>
-        <S.Title>회원 정보를 입력해주세요.</S.Title>
-
-        <S.MainContainer>
-          <TextInput
-            label="이메일 주소"
-            placeholder="이메일 주소를 입력해주세요."
-            type="text"
-            value={getValues('account.identification')}
-            disabled
-          />
-          <TextInput
-            label="비밀번호"
-            placeholder="영문, 숫자, 특수문자를 조합하여 8자 이상"
-            type="password"
-            errorMsg={errors.account?.password ? '영문, 숫자, 특수문자를 조합하여 8자 이상 입력해주세요.' : undefined}
-            {...register('account.password', {
-              required: '비밀번호를 입력해주세요',
-              pattern: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$%^&+=!?]).{8,}$/,
-            })}
-          />
-          <TextInput
-            label="비밀번호 확인"
-            placeholder="비밀번호 재입력"
-            type="password"
-            errorMsg={errors.account?.passwordConfirm ? '비밀번호가 일치하지 않습니다.' : undefined}
-            {...register('account.passwordConfirm', {
-              required: true,
-              pattern: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$%^&+=!?]).{8,}$/,
-              validate: value => getValues('account.password') === value || '비밀번호가 일치하지 않습니다.',
-            })}
-          />
-        </S.MainContainer>
-
-        <S.NextButtonWrapper>
-          <MainButton btnText="다음" disabled={!isDirty || !isValid} type="button" onClick={onNext} />
-        </S.NextButtonWrapper>
-      </S.Wrapper> */}
+        </footer>
+      </BottomButtonContainer>
     </>
   );
 }
