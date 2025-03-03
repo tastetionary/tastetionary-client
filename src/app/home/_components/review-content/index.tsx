cda93e88cf1a1c90bccae767a51ade06cfd56e43
hovelopin <kimhojin3714@naver.com>
2025-03-03 20:18:23 +0900

    fix: server fetch 적용 작업 완료

--- src/app/home/_components/review-content/index.tsx
+++ src/app/home/_components/review-content/index.tsx
@@ -1,13 +1,15 @@
-export default function ReviewContent() {
+export interface Reviews {
+  address: string;
+  name: string;
+  summary: string;
+}
+
+export default function ReviewContent({ address, name, summary }: Reviews) {
   return (
     <div className="border-b border-solid border-neutral-bg20 pb-20 pt-16">
-      <p className="title3 font-bold">식당명</p>
-      <p className="body3 text-neutral-bg60">주소주소주소주소주소주소주소주소주소주소주소주소주소주소주소</p>
-      <p className="body2 text-neutral-bg80">
-        리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용
-        리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용 리뷰 내용
-        리뷰 내용...
-      </p>
+      <p className="title3 font-bold">{name}</p>
+      <p className="body3 text-neutral-bg60">{address}</p>
+      <p className="body2 text-neutral-bg80">{summary}</p>
     </div>
   );
 }
