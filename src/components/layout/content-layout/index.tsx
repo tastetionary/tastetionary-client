6669bdda36198fb2ab699900e1989a606030246e
yejiiha <1126yezy@gmail.com>
2025-04-14 21:28:07 +0900

    feat: 비밀번호 재설정 페이지 연결, 기능 추가

--- src/components/layout/content-layout/index.tsx
+++ src/components/layout/content-layout/index.tsx
@@ -1,9 +1,11 @@
+import { cn } from '@/utils/styles.utils';
 import { ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
+  className?: string;
 }
 
-export default function ContentLayout({ children }: Props) {
-  return <div className="px-xl pb-[120px]">{children}</div>;
+export default function ContentLayout({ children, className }: Props) {
+  return <div className={cn('px-xl pb-[120px]', className)}>{children}</div>;
 }
