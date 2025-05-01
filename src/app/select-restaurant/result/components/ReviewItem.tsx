7b38766ec8c692eac50e74a3de2747be33b0b505
yejiiha <1126yezy@gmail.com>
2025-05-01 23:27:53 +0900

    feat: 리뷰 리액션 버튼 기능 추가

--- src/app/select-restaurant/result/components/ReviewItem.tsx
+++ src/app/select-restaurant/result/components/ReviewItem.tsx
@@ -1,11 +1,30 @@
+import restaurantRepository from '@/apis/restaurant';
+import { type RestaurantReview } from '@/apis/restaurant/recommend';
 import IC_DISLIKE from '@/assets/common/Icons/dislike.svg';
 import IC_LIKE from '@/assets/common/Icons/like.svg';
 import DefaultButton from '@/components/Button/DefaultButton';
 import CReviewReportBtn from '@/components/c-review-report-btn';
-import { GetRestaurantReviewRes } from '@/types/review';
+import useToken from '@/hooks/useToken';
+import { useSelectResultStore } from '@/store/useSelectResultStore';
+import { cn } from '@/utils/styles.utils';
+import { useMutation } from '@tanstack/react-query';
 import dayjs from 'dayjs';
 
-export default function ReviewItem({ user, createdAt, id, keywords, summary, opinion }: GetRestaurantReviewRes) {
+export default function ReviewItem({ user, createdAt, id, keywords, summary, userReaction }: RestaurantReview) {
+  const { restaurant, updateReviewReact } = useSelectResultStore();
+  const { token } = useToken();
+
+  const { mutate: updateReact } = useMutation({
+    mutationFn: restaurantRepository().reactRestaurantReview,
+    onSuccess: (_, { reactionType }) => {
+      updateReviewReact(id, reactionType);
+    },
+  });
+
+  const handleClickReviewReactBtn = (type: 'L' | 'D') => {
+    updateReact({ restaurantId: Number(restaurant?.id), reviewId: Number(id), reactionType: type, token });
+  };
+
   return (
     <div className="flex w-full flex-col gap-sm px-xl pb-lg pt-md not-last:border-b-1 not-last:border-solid not-last:border-neutral-bg20">
       <div className="flex items-center justify-between">
@@ -33,18 +52,20 @@ export default function ReviewItem({ user, createdAt, id, keywords, summary, opi
       <div className="flex w-full gap-xxs">
         <DefaultButton
           bgColor="gray"
-          customStyle={'py-12 flex gap-xs flex-grow' + (opinion === 'Y' ? ' default-btn selected ' : '')}
+          customStyle={cn('py-12 flex gap-xs flex-grow', userReaction === 'L' ? 'default-btn selected ' : '')}
+          onClick={() => handleClickReviewReactBtn('L')}
         >
           <IC_LIKE />
-          <span className="body2">도옴이 돼요</span>
+          <span className="body2">도움이 돼요</span>
         </DefaultButton>
 
         <DefaultButton
           bgColor="gray"
-          customStyle={'py-12 flex gap-xs flex-grow' + (opinion === 'N' ? ' default-btn selected ' : '')}
+          customStyle={cn('py-12 flex gap-xs flex-grow', userReaction === 'D' ? 'default-btn selected ' : '')}
+          onClick={() => handleClickReviewReactBtn('D')}
         >
           <IC_DISLIKE />
-          <span className="body2">도옴 안 돼요</span>
+          <span className="body2">도움 안 돼요</span>
         </DefaultButton>
       </div>
     </div>
