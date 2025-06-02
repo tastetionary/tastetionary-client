09473ae8d5ebb2f08cd149b28ff891d2eb7561aa
hovelopin <kimhojin3714@naver.com>
2025-06-02 20:09:31 +0900

    feat: 오늘 많이 고른 메뉴 api 연동 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import RecommendMenu from './_components/recommend-menu';
 
 export default async function Home() {
   const recentReviews = await homeRepository().getRecentReviews();
+  const recentPickedMenu = await homeRepository().getRecentPickedMenu();
 
   return (
     <CServerHeaderWithChildren title="맛셔너리" isHome isLogo>
@@ -20,7 +21,7 @@ export default async function Home() {
 
       <RecentReviews reviews={recentReviews} />
 
-      <RecommendMenu />
+      <RecommendMenu recentPickedMenu={recentPickedMenu} />
 
       <footer>
         <FooterLinks />
