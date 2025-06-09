01ac6dbe9db3613efea080fb98db6eda32343be9
Taehoya <cth6809@naver.com>
2025-06-09 16:39:22 -0500

    fix: fix sentry config

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const SentryWebpackPluginOptions = {
 
   // Suppresses source map uploading logs during build
   silent: true,
-  org: 'tastetionary',
+  org: 'tastionary',
   project: 'taste-client',
   authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_KEY, // An auth token is required for uploading source maps.
 };
