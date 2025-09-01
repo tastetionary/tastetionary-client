3971da4bb101bfebfaed591136136a74a5c93758
hovelopin <kimhojin3714@naver.com>
2025-09-01 18:55:55 +0900

    fix: standalone 설정 제거 작업

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ const SentryWebpackPluginOptions = {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // output: 'standalone',
   staticPageGenerationTimeout: 90,
   compiler: {
     styledComponents: true,
