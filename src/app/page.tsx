e8c3983afa9a25bfb1465e166b9ad28fc0e26e94
hovelopin <kimhojin3714@naver.com>
2025-09-15 21:47:32 +0900

    feat: errorboundary 처리 추가 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-import { homeRepository } from '@/apis/home';
 import CServerHeaderWithChildren from '@/components/c-server-header-with-children';
+import { ErrorBoundary } from 'react-error-boundary';
 import BannerSlider from './_components/banner-swiper';
+import ErrorFallback from './_components/error-fallback';
 import FooterLinks from './_components/footer-links';
 import LocationSection from './_components/location-section';
 import MenuSelection from './_components/menu-selection';
-import RecentReviews from './_components/recent-reivews';
-import RecommendMenu from './_components/recommend-menu';
+import RecentReviewsFetcherWrapper from './_components/wrapper/recent-reviews-fetcher-wrapper';
+import RecommendMenuFetcherWrapper from './_components/wrapper/recommend-menu-fetcher-wrapper';
 
 // 동적 렌더링 강제 (캐싱 비활성화)
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  const recentReviews = await homeRepository().getRecentReviews();
-  const recentPickedMenus = await homeRepository().getRecentPickedMenus();
-
   return (
     <CServerHeaderWithChildren title="맛셔너리" isHome isLogo>
       <LocationSection />
@@ -22,9 +20,10 @@ export default async function Home() {
 
       <BannerSlider />
 
-      <RecentReviews reviews={recentReviews} />
-
-      <RecommendMenu menus={recentPickedMenus} />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <RecentReviewsFetcherWrapper />
+        <RecommendMenuFetcherWrapper />
+      </ErrorBoundary>
 
       <footer>
         <FooterLinks />
